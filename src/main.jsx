f5cdfb7e88a10f03b055682352bfc3d33a7c2817
jhernandez4 <jokhernandez120744@gmail.com>
2025-05-05 23:01:05 -0700

    style: update sign up page folder name

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter } from 'react-router'
 import { RouterProvider } from 'react-router-dom'
-import SignUpPage from './pages/SignUp/SignUpPage.jsx'
+import SignUpPage from './pages/SignUpPage/SignUpPage.jsx'
 
 const router = createBrowserRouter([
   {
