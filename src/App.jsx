f9ae4281e87059349073cfbae6bf1c0a2fbfec02
jhernandez4 <jokhernandez120744@gmail.com>
2025-05-05 18:48:53 -0700

    fix(app page): use global color variables and change selector names

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App() {
       <div className="landing-card">
         <h1>Welcome</h1>
         <p>Please choose an option below:</p>
-        <div className="button-group">
-          <a href="/signup" className="btn primary-btn">Sign Up</a>
-          <a href="/login" className="btn secondary-btn">Log In</a>
+        <div className="landing-btn-group">
+          <a href="/signup" className="landing-btn primary-landing-btn">Sign Up</a>
+          <a href="/login" className="landing-btn secondary-landing-btn">Log In</a>
         </div>
       </div>
     </div>
