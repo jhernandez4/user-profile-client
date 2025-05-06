79ab72e0c9acb533fc4a8d3d9fa00932aaced5e0
jhernandez4 <jokhernandez120744@gmail.com>
2025-05-05 18:12:06 -0700

    feat: create login page

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,15 @@ import './App.css'
 function App() {
 
   return (
-    <div>
-      <button>Hello World!</button>
-      <button>Press</button>
+    <div className="landing-container">
+      <div className="landing-card">
+        <h1>Welcome</h1>
+        <p>Please choose an option below:</p>
+        <div className="button-group">
+          <a href="/signup" className="btn primary-btn">Sign Up</a>
+          <a href="/login" className="btn secondary-btn">Log In</a>
+        </div>
+      </div>
     </div>
   )
 }
